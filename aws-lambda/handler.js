17f138d3f451b7442d4fae8219871d9f68019896
MatteoGioioso <gioioso.matteo@gmail.com>
2021-04-02 09:10:53 +0700

    add lambda

--- aws-lambda/handler.js
+++ aws-lambda/handler.js
@@ -3,7 +3,14 @@
 const uuid = require('uuid');
 const axios = require('axios');
 const AWS = require('aws-sdk');
+const {RebugitSDK} = require('rbi-nodejs-agent');
 
+const rbi = new RebugitSDK({
+    apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiIzMTI3ODc1MC05NDUyLTQ4OGYtODNjNy1mYTYxZDcxZDcxZTQiLCJ0ZW5hbnRJZCI6IjFjNjJiZDc0LTQwMTYtNDBkZS1hNGJhLTZmMGZkYWU1ZjBmNyJ9.IYLawG70gdl5D3_OHZVV65KG0OEMh-CnqFBKbzEKoqo',
+    collector: {
+        collectorBaseUrl: "dev.api.rebugit.com"
+    }
+})
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const secretsManager = new AWS.SecretsManager({
     region: process.env.AWS_REGION
@@ -11,22 +18,23 @@ const secretsManager = new AWS.SecretsManager({
 
 const getApiKey = async () => {
     const resp = await secretsManager.getSecretValue({SecretId: process.env.SECRET_NAME}).promise()
-    return resp.SecretString;
+    return JSON.parse(resp.SecretString).apiKey;
 }
 
 const getCity = async (lat, lon, apiKey) => {
+    const url = `https://www.mapquestapi.com/geocoding/v1/reverse?key=${apiKey}&location=${lat}%2C${lon}&outFormat=json&thumbMaps=false`
     const resp = await axios({
         method: 'get',
-        url: `https://www.mapquestapi.com/geocoding/v1/reverse?key=${apiKey}&location=${lat}%2C${lon}&outFormat=json&thumbMaps=false`
+        url
     })
 
     return {
-        city: resp.data.results[0].location[0].adminArea5,
-        address: resp.data.results[0].location[0].street
+        city: resp.data.results[0].locations[0].adminArea5,
+        address: resp.data.results[0].locations[0].street
     }
 }
 
-const weather = async (lat, long) => {
+const getForecastWeather = async (lat, long) => {
     const resp = await axios({
         method: "get",
         url: `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`,
@@ -78,16 +86,17 @@ const getWeatherEmoji = (result, forecastType) => {
         case "clearsky_day":
             return "🌞"
         case "cloudy_day":
+        case "cloudy":
             return " ☁"
         default:
             return "⛈"
     }
 }
 
-module.exports.hello = async (event) => {
+module.exports.getWeather = rbi.AWSLambda().lambdaHandler(async (event) => {
     try {
         const data = JSON.parse(event.body);
-
+        console.log(JSON.stringify(data, null, 2))
         if (data.lat === "" || data.long === "") {
             throw new Error("latitude or longitude is missing")
         }
@@ -97,11 +106,20 @@ module.exports.hello = async (event) => {
         }
 
         const apiKey = await getApiKey();
+        console.log("got api key")
+
         const city = await getCity(data.lat, data.long, apiKey)
-        const forecast = await weather(data.lat, data.long)
+        console.log(JSON.stringify(city, null, 2))
+
+        const forecast = await getForecastWeather(data.lat, data.long)
+        console.log(JSON.stringify(forecast, null, 2))
+
         const id = await storeForecast(forecast, city)
         const result = await getForecast(id)
+        console.log(JSON.stringify(result, null, 2))
+
         const weather = getWeatherEmoji(result, data.forecast)
+        console.log("Weather: ", weather)
         // create a response
         return {
             statusCode: 200,
@@ -109,11 +127,19 @@ module.exports.hello = async (event) => {
                 forecast: weather,
                 position: result.city
             }),
+            headers: {
+                "Access-Control-Allow-Origin" : "*"
+            },
         };
     } catch (e) {
+        console.log(e.message)
+        console.log(e.stack)
         return {
             statusCode: 500,
-            body: e.message
+            body: e.message,
+            headers: {
+                "Access-Control-Allow-Origin" : "*"
+            },
         }
     }
-}
+})
